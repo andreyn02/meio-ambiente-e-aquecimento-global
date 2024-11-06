0abf4c957a373cd8e12c58f23aa32cc695a06e53
andreyn02 <andrey.serconhuk.mendes@escola.pr.gov.br>
2024-11-06 18:34:18 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var limg = 3072;
+var himg = 1066;
+var ximg = 0;
+var yimg = 20;
+var img;
+var img2; 
+var img3;
+var img4;
+var img5;
+var img6;
+var img7;
+
+function preload() {
+  img = loadImage("aquecimento global3.png");
+  img2 = loadImage("tabela.png")
+  img3 = loadImage("açoes humanas.jpg")
+  img4 = loadImage("estufa.jpg")
+  img5 = loadImage("aquecimento global2.png")
+  img6 = loadImage("salvação.jpg")
+  img7 = loadImage("Aquecimento-Global-14-770x441.jpg")
+}
